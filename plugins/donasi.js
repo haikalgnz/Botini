77cae3fca9860870ce0c60b45d0ff4aa07f3b1b8
haikalgnz <haikalofc8@gmail.com>
2021-11-10 12:33:00 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ GoPay: 628112958665
-├ OVO: 628112958665
-├ Dana: 628112958665
-├ Pulsa (XL): 6281943265086
+├ GoPay: 085363810820 
+├ OVO: - 
+├ Dana: 089603042257 
+├ Pulsa (Tri): 089603042257 
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
